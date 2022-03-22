3923990fcfd5d1ede0debbd7fe49dc7c2d527304
surajdm123 <surajdm123@gmail.com>
2022-03-22 17:30:54 -0400

    Refactored file structure

---
+++ src/config/DatabaseConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+public class DatabaseConfig {
+
+    public static final int PORT = 3306;
+
+    public static final String USERNAME = "root";
+
+    public static final String PASSWORD = "root";
+
+    public static final String DATABASE_NAME = "test";
+
+    public static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+
+    public static String HOST = "localhost";
+
+    public static String DATABASE_URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE_NAME;
+
+    // MariaDB Configuration
+//    public static final String USERNAME = "sdevath";
+//
+//    public static final String PASSWORD = "Password";
+//
+//    public static final String DATABASE_NAME = "sdevath";
+//
+//    public static String DRIVER_NAME = "org.mariadb.jdbc.Driver";
+//
+//    public static String HOST = "classdb2.csc.ncsu.edu";
+//
+//    public static String DATABASE_URL = "jdbc:mariadb://" + HOST + ":" + PORT + "/" + DATABASE_NAME;
+
+}
