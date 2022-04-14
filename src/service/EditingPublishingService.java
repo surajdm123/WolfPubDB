2726dde9e0728e7a714b0ffdefc14b7386bda984
Manvita Balachandra <manvitanaik11@gmail.com>
2022-04-14 16:34:26 -0400

    COMMITS to Editing Publishing Service

--- src/service/EditingPublishingService.java
+++ src/service/EditingPublishingService.java
@@ -5,11 +5,13 @@ import constants.MYSQL_CONSTANTS;
 import java.sql.*;
 import java.util.Scanner;
 
+//Class definition for EditingPublishingService
 public class EditingPublishingService {
 
     Scanner scanner = new Scanner(System.in);
     ResultSetService resultSetService = new ResultSetService();
 
+    //Run function to handle main option for all functionalities offered by EditingPublishingService
     public void run(final Connection connection) {
 
         try {
@@ -35,6 +37,7 @@ public class EditingPublishingService {
 
                 System.out.println("Enter your choice:");
 
+                //Switch cases to call functions involved in EditingPublishingService
                 int choice = scanner.nextInt();
                 scanner.nextLine();
                 switch (choice) {
@@ -82,14 +85,17 @@ public class EditingPublishingService {
                     break;
                 }
             }
+            //catch Exception when error occurs
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e.getMessage());
         }
 
     }
 
+    //Function to view Publication Information
     public boolean getPublicationInformation(final Connection connection) {
 
+        //Criterion to view publication information on
         System.out.println("1. Find by ID");
         System.out.println("2. Find by Title\n");
         System.out.println("3. Return to Main Menu\n");
@@ -101,8 +107,10 @@ public class EditingPublishingService {
 
             switch (choice) {
                 case 1:
+                    //Take publication ID as input.
                     System.out.println("Enter Publication ID: ");
                     int publicationId = scanner.nextInt();
+                    // Displays publication table details, author involved, for a specific type of publication where staff's id is same as author's id. This display is grouped by publication id.
                     final String sqlQuery = "SELECT publication.*, GROUP_CONCAT(staff.name) as 'Author Name(s)' FROM publication NATURAL JOIN writes JOIN staff ON writes.sid = staff.sid WHERE pid=? GROUP BY publication.pid;";
                     PreparedStatement statement = connection.prepareStatement(sqlQuery);
                     statement.setInt(1, publicationId);
@@ -113,6 +121,7 @@ public class EditingPublishingService {
                     scanner.nextLine();
                     System.out.println("Enter Publication Title: ");
                     String title = scanner.nextLine();
+                    // Displays publication table details, author involved, for a specific type of publication where staff's id is same as author's id. This display is grouped by publication id.
                     final String query = "SELECT publication.*, GROUP_CONCAT(staff.name) as 'Author Name(s)' FROM publication NATURAL JOIN writes JOIN staff ON writes.sid = staff.sid WHERE publication.title=? GROUP BY publication.pid;";
                     PreparedStatement preparedStatement = connection.prepareStatement(query);
                     preparedStatement.setString(1, title);
@@ -135,12 +144,14 @@ public class EditingPublishingService {
     public boolean getAllPublicationsAssignedToEditor(final Connection connection) {
 
         System.out.println("Details about all the Editors: \n");
+        //Displays staff details for staff whose title equals "Editor"
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * FROM staff where title = 'Editor';");
 
         System.out.println("Enter the editor ID: ");
         final int editorId = scanner.nextInt();
 
         try {
+            //Displays publication details,status of publication being edited by editor
             final String sqlQuery = "SELECT publication.*, edits.status FROM publication NATURAL JOIN edits where sid=?;";
             PreparedStatement statement = connection.prepareStatement(sqlQuery);
             statement.setInt(1, editorId);
@@ -160,6 +171,7 @@ public class EditingPublishingService {
     public boolean addChapter(final Connection connection) {
 
         System.out.println("Publications in the database:");
+        // Displays publication details, number of pages of the book, edition details for an edition of a book
         resultSetService.runQueryAndPrintOutput(connection, "SELECT publication.*, book.number_of_pages, editions.* from publication NATURAL JOIN book NATURAL JOIN editions");
 
         System.out.println("Enter the Chapter Number: ");
@@ -181,6 +193,7 @@ public class EditingPublishingService {
         try {
             connection.setAutoCommit(false);
             try {
+                // Insert chapter details into chapter table including chapter nummber, publication id, edition number, chapter name, text of the chapter
                 final String sqlQuery = "INSERT INTO `chapters` (`chapter_number`, `pid`, `edition_number`, `chapter_name`, `text`) VALUES (?, ?, ?, ?, ?);";
                 PreparedStatement statement = connection.prepareStatement(sqlQuery);
                 statement.setInt(1, chapterNumber);
@@ -210,6 +223,7 @@ public class EditingPublishingService {
     public boolean addArticle(final Connection connection) {
 
         System.out.println("Periodic publications present in the database:");
+        // Displays publication details, issue id, issue title for an issue belonging to a publication
         resultSetService.runQueryAndPrintOutput(connection, "SELECT publication.*, issues.issueId, issues.issue_title from publication NATURAL JOIN periodic_publication JOIN issues ON issues.pid = periodic_publication.pid;");
 
         System.out.println("Enter the publication ID: ");
@@ -231,6 +245,7 @@ public class EditingPublishingService {
         try {
             connection.setAutoCommit(false);
             try {
+                //Insert article details into article table including publication id, issue id, text of the article, article date, article name
                 final String sqlQuery = "INSERT INTO `articles` (`pid`, `issueId`, `text`, `date`, `name`) VALUES (?, ?, ?, ?, ?);";
                 PreparedStatement statement = connection.prepareStatement(sqlQuery);
                 statement.setInt(1, publicationId);
@@ -260,11 +275,11 @@ public class EditingPublishingService {
     public boolean assignEditorToPublication(final Connection connection) {
 
         System.out.println("Details about all the Editors: \n");
-
+        // Displays details of the staff for Editor type only
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * FROM staff where title = 'Editor';");
 
         System.out.println("\nDetails about all the publications: \n");
-
+        // Displays all publication details
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * FROM publication;");
 
         System.out.println("\nEnter the publication ID: ");
@@ -277,7 +292,7 @@ public class EditingPublishingService {
             connection.setAutoCommit(false);
 
             try {
-
+                //Insert details of editor responsible for editing a publication
                 final String sqlQuery = "INSERT INTO `edits` (`sid`, `pid`) VALUES (?, ?);";
                 PreparedStatement statement = connection.prepareStatement(sqlQuery);
                 statement.setInt(1, editorId);
@@ -306,6 +321,7 @@ public class EditingPublishingService {
     public boolean deleteChapter(final Connection connection) {
 
         System.out.println("Chapters of publications in the database:");
+        //Display all chapter details
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * from chapters;");
 
         System.out.println("\nEnter the following details to delete a chapter.\n");
@@ -325,7 +341,7 @@ public class EditingPublishingService {
             connection.setAutoCommit(false);
 
             try {
-
+                //Delete a specific chapter with a chapter name, publication if and edition number.
                 final String sqlQuery = "DELETE FROM `chapters` WHERE (`chapter_number` = ?) and (`pid` = ?) and (`edition_number` = ?);";
                 PreparedStatement statement = connection.prepareStatement(sqlQuery);
                 statement.setInt(1, chapterNumber);
@@ -361,6 +377,7 @@ public class EditingPublishingService {
     public boolean deleteArticle(final Connection connection) {
 
         System.out.println("Articles present in the database:");
+        //Display details of all articles
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * FROM articles;");
 
         System.out.println("\nEnter the following information to delete the article.\n");
@@ -378,7 +395,7 @@ public class EditingPublishingService {
             connection.setAutoCommit(false);
 
             try {
-
+                //Delete entries of articles for a specific article id and pid and issueId
                 final String sqlQuery = "DELETE FROM `articles` WHERE (`articleId` = ?) and (`pid` = ?) and (`issueId` = ?);";
                 PreparedStatement statement = connection.prepareStatement(sqlQuery);
                 statement.setInt(1, articleNumber);
@@ -413,6 +430,7 @@ public class EditingPublishingService {
     public boolean updateBookPublication(Connection connection) {
 
         System.out.println("Books in the database:");
+        //Displays publication details, number of pages of a book for a book under a specific publication
         resultSetService.runQueryAndPrintOutput(connection, "SELECT publication.*, book.number_of_pages FROM publication NATURAL JOIN book;");
 
         System.out.println("Enter Book Publication ID you want to update: ");
@@ -436,6 +454,7 @@ public class EditingPublishingService {
                     case 1:
                         System.out.println("Enter the new Title: \t");
                         final String newTitle = scanner.nextLine();
+                        //Update publication details while setting a new Title for a publication
                         final String titleUpdateSqlQuery = "UPDATE publication SET title = ? WHERE pid = ?;";
                         PreparedStatement titleUpdateStatement = connection.prepareStatement(titleUpdateSqlQuery);
                         titleUpdateStatement.setString(1, newTitle);
@@ -448,6 +467,7 @@ public class EditingPublishingService {
                     case 2:
                         System.out.println("Enter the new Publication Date (yyyy-mm-dd): \t");
                         final String newDate = scanner.nextLine();
+                        // Update publication details by setting publication date to a new date for a specific publication
                         final String dateUpdateSqlQuery = "UPDATE publication SET publication_date = ? WHERE pid = ?;";
                         PreparedStatement dateUpdateStatement = connection.prepareStatement(dateUpdateSqlQuery);
                         dateUpdateStatement.setString(1, newDate);
@@ -460,6 +480,7 @@ public class EditingPublishingService {
                     case 3:
                         System.out.println("Enter the new genre: \t");
                         final String newGenre = scanner.nextLine();
+                        // Update publication and set a new genre for a specific publication
                         final String genreUpdateSqlQuery = "UPDATE publication SET genre = ? WHERE pid = ?;";
                         PreparedStatement genreUpdateStatement = connection.prepareStatement(genreUpdateSqlQuery);
                         genreUpdateStatement.setString(1, newGenre);
@@ -472,6 +493,7 @@ public class EditingPublishingService {
                     case 4:
                         System.out.println("Enter the new number of pages: \t");
                         final int newNumberOfPages = scanner.nextInt();
+                        //Update the number of pages for a specific publication id
                         final String pagesUpdateSqlQuery = "UPDATE book SET number_of_pages = ? WHERE pid = ?;";
                         PreparedStatement pagesUpdateStatement = connection.prepareStatement(pagesUpdateSqlQuery);
                         pagesUpdateStatement.setInt(1, newNumberOfPages);
@@ -514,6 +536,7 @@ public class EditingPublishingService {
         final int numberOfPages = scanner.nextInt();
 
         System.out.println("Authors in the database:");
+        //Display details for all staff members who are of "author" type.
         resultSetService.runQueryAndPrintOutput(connection, "SELECT * FROM staff where title='Author'");
 
         System.out.println("Enter Author Id:");
@@ -522,7 +545,7 @@ public class EditingPublishingService {
         try {
             connection.setAutoCommit(false);
             try {
-
+                // insert publication details including title, publication date, genre, publication type into publication table.
                 String sqlStatement1 = "INSERT INTO `publication` (`title`, `publication_date`, `genre`,`publication_type`) VALUES (?,?,?,?);";
                 PreparedStatement statement1 = connection.prepareStatement(sqlStatement1, Statement.RETURN_GENERATED_KEYS);
                 statement1.setString(1, title);
@@ -542,12 +565,14 @@ public class EditingPublishingService {
                     throw new SQLException("Could not insert into table publication");
                 }
 
+                //Insert book details like publication id and number of pages
                 String sqlStatement2 = "INSERT INTO `book` (`pid`, `number_of_pages`) VALUES (?,?);";
                 PreparedStatement statement2 = connection.prepareStatement(sqlStatement2);
                 statement2.setInt(1, pid);
                 statement2.setInt(2, numberOfPages);
                 statement2.executeUpdate();
 
+                //Insert author details responsible for writing a specific publication type.category
                 String sqlStatement3 = "INSERT INTO `writes` (`sid`, `pid`) VALUES (?,?);";
                 PreparedStatement statement3 = connection.prepareStatement(sqlStatement3);
                 statement3.setInt(1, authorId);
@@ -588,8 +613,8 @@ public class EditingPublishingService {
 
         try {
             connection.setAutoCommit(false);
-            try {
-
+            try{
+                //Insert publication details like title, publication date, genre, publication type into publication table entries.
                 String sqlStatement1 = "INSERT INTO `publication` (`title`, `publication_date`, `genre`,`publication_type`) VALUES (?,?,?,?);";
                 PreparedStatement statement1 = connection.prepareStatement(sqlStatement1, Statement.RETURN_GENERATED_KEYS);
                 statement1.setString(1, title);
@@ -609,12 +634,14 @@ public class EditingPublishingService {
                     throw new SQLException("Could not insert into table publication");
                 }
 
+                //Insert periodic publication details like publication id and periodicity into the publication table.
                 String sqlStatement2 = "INSERT INTO `periodic_publication` (`pid`, `periodicty`) VALUES (?,?);";
                 PreparedStatement statement2 = connection.prepareStatement(sqlStatement2);
                 statement2.setInt(1, pid);
                 statement2.setString(2, periodicity);
                 statement2.executeUpdate();
 
+                //Insert author details responsible for writing a specific publication type/category
                 String sqlStatement3 = "INSERT INTO `writes` (`sid`, `pid`) VALUES (?,?);";
                 PreparedStatement statement3 = connection.prepareStatement(sqlStatement3);
                 statement3.setInt(1, authorId);
@@ -641,6 +668,7 @@ public class EditingPublishingService {
     public boolean updatePeriodicPublication(Connection connection) {
 
         System.out.println("Periodic Publications in the database:");
+        //Display publication, periodicity for a periodic publication type of publication entry
         resultSetService.runQueryAndPrintOutput(connection, "SELECT publication.*, periodic_publication.periodicity FROM publication NATURAL JOIN periodic_publication;");
 
         System.out.println("Enter Periodic Publication ID you want to update: ");
@@ -664,6 +692,7 @@ public class EditingPublishingService {
                     case 1:
                         System.out.println("Enter the new Title: \t");
                         final String newTitle = scanner.nextLine();
+                        //Update the title of the publication for a specific publication id.
                         final String titleUpdateSqlQuery = "UPDATE publication SET title = ? WHERE pid = ?;";
                         PreparedStatement titleUpdateStatement = connection.prepareStatement(titleUpdateSqlQuery);
                         titleUpdateStatement.setString(1, newTitle);
@@ -676,6 +705,7 @@ public class EditingPublishingService {
                     case 2:
                         System.out.println("Enter the new Publication Date (yyyy-mm-dd): \t");
                         final String newDate = scanner.nextLine();
+                        //Update the date of the publication for a specific publication id.
                         final String dateUpdateSqlQuery = "UPDATE publication SET publication_date = ? WHERE pid = ?;";
                         PreparedStatement dateUpdateStatement = connection.prepareStatement(dateUpdateSqlQuery);
                         dateUpdateStatement.setString(1, newDate);
@@ -688,6 +718,7 @@ public class EditingPublishingService {
                     case 3:
                         System.out.println("Enter the new genre: \t");
                         final String newGenre = scanner.nextLine();
+                        //Update the genre of the publication for a specific publication id.
                         final String genreUpdateSqlQuery = "UPDATE publication SET genre = ? WHERE pid = ?;";
                         PreparedStatement genreUpdateStatement = connection.prepareStatement(genreUpdateSqlQuery);
                         genreUpdateStatement.setString(1, newGenre);
@@ -700,6 +731,7 @@ public class EditingPublishingService {
                     case 4:
                         System.out.println("Enter the new Periodicity (Daily, Weekly, Monthly): \t");
                         final String periodicity = scanner.nextLine();
+                        //Update the periodicity of the periodic publication for a specific publication id.
                         final String periodicityUpdateSqlQuery = "UPDATE periodic_publication SET periodicity = ? WHERE pid = ?;";
                         PreparedStatement periodicityUpdateStatement = connection.prepareStatement(periodicityUpdateSqlQuery);
                         periodicityUpdateStatement.setString(1, periodicity);
